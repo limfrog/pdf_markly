22df3e9c97799c95395c89a2a60df3f1b56ac426
limfrog <limfrog75@gmail.com>
2026-09-17 18:37:06 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'pdf-markly-v3';
-const CDN_CACHE = 'pdf-markly-cdn-v3';
+const CACHE_NAME = 'pdf-markly-v4';
+const CDN_CACHE = 'pdf-markly-cdn-v4';
 const ASSETS = ['./', './index.html', './manifest.json',
   './favicon.png', './icon-192.png', './icon-512.png'];
 
